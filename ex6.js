446c3faffe82d2ad3ff74e9c241920f7adbc1839
Rawr <All-Cashmere@users.noreply.github.com>
2019-01-15 17:49:20 -0500

    Create ex6.js

---
+++ ex6.js
@@ -0,0 +1,29 @@
+
+class Car {
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+    details(){
+        return `Model: ${this.name} Engine ${this.year}`;
+    }
+}
+const car2 = new Car('Pontiac Firebird', 1976);
+console.log(car2.details());
+
+class Sedan extends Car{
+    constructor(name, year, balance){
+        super(name, year);
+        this.balance = balance
+    }
+
+    info(){
+        return `${this.name} has a balance of $${this.balance.toFixed(2)}`
+
+    }
+}
+const sedan = new Sedan('Volvo SD', 2018, 30000);
+console.log(sedan.info());
+
+//Model: Pontiac Firebird Engine 1976
+//Volvo SD has a balance of $30000.00
